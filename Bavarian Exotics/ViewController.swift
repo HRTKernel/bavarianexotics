41261da4373cd594bef1c58ed5850e308d8b9c46
HRTKernel <admin@hrt-projekt.de>
2017-09-20 14:54:02 +0200

    Upload Build 1.0.0
    Build with swift 4 and xcode 9
    Tested with iOS11

---
+++ Bavarian Exotics/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Bavarian Exotics
+//
+//  Created by Maik Diebenkorn on 20.09.17.
+//  Copyright © 2017 Maik Diebenkorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        let url = URL(string: "https://bavarianexotics.de")
+        if let unwrappedURL = url {
+            
+            let request = URLRequest(url: unwrappedURL)
+            let session = URLSession.shared
+            
+            let task = session.dataTask(with: request) { (data, response, error) in
+                
+                if error == nil {
+                    
+                    self.webView.loadRequest(request)
+                    
+                } else {
+                    
+                    print("ERROR: \(String(describing: error))")
+                    
+                }
+                
+            }
+            
+            task.resume()
+            
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
